ba8a9d6a9e3ec7e5dcfa05b694357725cea4a16b
lzm <>
2020-05-22 16:56:23 +0800

    1

--- src/main/java/com/fm/demo/login/controller/LoginController.java
+++ src/main/java/com/fm/demo/login/controller/LoginController.java
@@ -2,7 +2,23 @@ package com.fm.demo.login.controller;
 
 import com.fm.demo.common.ApiBaseController;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import lombok.Data;
+
 @ApiBaseController
 public class LoginController {
-    
+    @Data
+    public static class User{
+        private String username;
+        private String password;
+    }
+    @PostMapping("/login")
+    public boolean login(@RequestBody User user) {
+        if(user != null) {
+            return true;
+        }
+        return false;
+    }
 }
