111341600ad6f38aa440c58dfb66daa2b2239c92
lzm <lxzzlxz@163.com>
2020-06-30 16:50:17 +0800

    1

--- src/main/java/com/fm/demo/common/Scheduler.java
+++ src/main/java/com/fm/demo/common/Scheduler.java
@@ -11,7 +11,7 @@ public class Scheduler {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     //每隔2秒执行一次
     /**
-     * @Scheduled(fixedRate=3000)：上一次开始执行时间点后3秒再次执行；
+        @Scheduled(fixedRate=3000)：上一次开始执行时间点后3秒再次执行；
         @Scheduled(fixedDelay=3000)：上一次执行完毕时间点3秒再次执行；
         @Scheduled(initialDelay=1000, fixedDelay=3000)：第一次延迟1秒执行，然后在上一次执行完毕时间点3秒再次执行；
         @Scheduled(cron="* * * * * ?")：按cron规则执行；
@@ -23,16 +23,15 @@ public class Scheduler {
 
     //每天3：05执行
     /**
-     * cron规则：
-     *  秒（0~59）
+     cron规则：
+        秒（0~59）
         分钟（0~59）
         小时（0~23）
         天（月）（0~31，但是你需要考虑你月的天数）
         月（0~11）
         天（星期）（1~7 1=SUN 或 SUN，MON，TUE，WED，THU，FRI，SAT）
         年份（1970－2099）
-
-
+     举例：
         0 0 10,14,16 * * ? 每天上午10点，下午2点，4点
         0 0/30 9-17 * * ?   朝九晚五工作时间内每半小时
         0 0 12 ? * WED 表示每个星期三中午12点 
